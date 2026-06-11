03d39bb07b0d4e2db0dcb4d6b58671e04c388af5
francovega690-cell <francovega690@gmail.com>
2026-06-11 02:12:25 -0300

    ya anda el buscador

--- assets/js/app.js
+++ assets/js/app.js
@@ -28,10 +28,10 @@ const obtenerUnPersonajeLs = async (idPersonaje) => {
   }
 };
 
-const cargarPersonajesLs = async (id) => {
-  personajes = await obtenerPersonajesLs();
-  console.log(personajes[0]);
-  personajes.forEach((element) => {
+const mostrarPersonajes = (lista) => {
+  contenedor.innerHTML = "";
+
+  lista.forEach((element) => {
     contenedor.innerHTML += `
     <div class="card" style="width: 18rem">
       <img
@@ -42,94 +42,88 @@ const cargarPersonajesLs = async (id) => {
       <div class="card-body">
         <h5 class="card-title">${element.name}</h5>
         <p class="card-text">
-             ${element.occupation}
-            </p> 
+          ${element.occupation}
+        </p>
         <p class="card-text">
-              ${element.status}
-            </p>
-            <button
-        id="modal"
-        type="button"
-        class="btn btn-primary"
-        data-bs-toggle="modal"
-        data-bs-target="#modal-${element.id}"
-      >
-        Ver más
-      </button>
+          ${element.status}
+        </p>
+
+        <button
+          type="button"
+          class="btn btn-primary"
+          data-bs-toggle="modal"
+          data-bs-target="#modal-${element.id}"
+        >
+          Ver más
+        </button>
       </div>
     </div>
+
     <div
-        class="modal fade"
-        id="modal-${element.id}"
-        data-bs-backdrop="static"
-        data-bs-keyboard="false"
-        tabindex="-1"
-        aria-labelledby="staticBackdropLabel"
-        aria-hidden="true"
-      >
-        <div class="modal-dialog">
-          <div class="modal-content">
-            <div class="modal-header">
-              <h1 class="modal-title fs-5" id="${element.id}">
-                ${element.name}
-              </h1>
-              <button
-                type="button"
-                class="btn-close"
-                data-bs-dismiss="modal"
-                aria-label="Close"
-              ></button>
-            </div>
+      class="modal fade"
+      id="modal-${element.id}"
+      data-bs-backdrop="static"
+      data-bs-keyboard="false"
+      tabindex="-1"
+      aria-hidden="true"
+    >
+      <div class="modal-dialog">
+        <div class="modal-content">
+          <div class="modal-header">
+            <h1 class="modal-title fs-5">
+              ${element.name}
+            </h1>
 
-            <div class="modal-body">
-               
-               <img src="https://cdn.thesimpsonsapi.com/500${element.portrait_path}"
-                class="img-fluid mb-3"
-                alt="${element.name}"
-               >
+            <button
+              type="button"
+              class="btn-close"
+              data-bs-dismiss="modal"
+            ></button>
+          </div>
 
-              <p><strong>Nombre:</strong> ${element.name}</p>
-              <p><strong>Edad:</strong> ${element.age}</p>
-              <p><strong>Género:</strong> ${element.gender}</p>
-              <p><strong>Ocupación:</strong> ${element.occupation}</p>
-              <p><strong>Estado:</strong> ${element.status}</p>
-              <p><strong>Frases:</strong> ${element.phrases}</p>
-             
-              </div>
-            </div>
+          <div class="modal-body">
+            <img
+              src="https://cdn.thesimpsonsapi.com/500${element.portrait_path}"
+              class="img-fluid mb-3"
+              alt="${element.name}"
+            >
 
-            <div class="modal-footer">
-              <button
-                type="button"
-                class="btn btn-secondary"
-                data-bs-dismiss="modal"
-              >
-                Cerrar
-              </button>
-            </div>
+            <p><strong>Nombre:</strong> ${element.name}</p>
+            <p><strong>Edad:</strong> ${element.age}</p>
+            <p><strong>Género:</strong> ${element.gender}</p>
+            <p><strong>Ocupación:</strong> ${element.occupation}</p>
+            <p><strong>Estado:</strong> ${element.status}</p>
+            <p><strong>Frases:</strong> ${element.phrases}</p>
+          </div>
+
+          <div class="modal-footer">
+            <button
+              type="button"
+              class="btn btn-secondary"
+              data-bs-dismiss="modal"
+            >
+              Cerrar
+            </button>
           </div>
         </div>
       </div>
-  `;
-  });
-};
-const mostrarPersonajes = (lista) => {
-  contenedor.innerHTML = "";
-
-  lista.forEach((element) => {
-    contenedor.innerHTML += `
-      <div class="card m-2" style="width: 18rem">
-        <img
-          src="https://cdn.thesimpsonsapi.com/500${element.portrait_path}"
-          class="card-img-top"
-          alt="${element.name}"
-        >
-        <div class="card-body">
-          <h5 class="card-title">${element.name}</h5>
-          <p class="card-text">${element.occupation}</p>
-        </div>
-      </div>
+    </div>
     `;
   });
 };
+
+const cargarPersonajesLs = async () => {
+  personajes = await obtenerPersonajesLs();
+  mostrarPersonajes(personajes);
+};
+
+buscador.addEventListener("input", (e) => {
+  const texto = e.target.value.toLowerCase();
+
+  const filtrados = personajes.filter((personaje) =>
+    personaje.name.toLowerCase().includes(texto),
+  );
+
+  mostrarPersonajes(filtrados);
+});
 cargarPersonajesLs();
